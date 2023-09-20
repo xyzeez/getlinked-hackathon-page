631faf2e9baaacb49e77e058c3e1938bb380e86e
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-09-20 19:02:43 +0300

    Modified nav functionality and fixed width issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,27 @@ const btnOpenHamburger = document.querySelector('#openHamburger');
 const btnCloseHamburger = document.querySelector('#closeHamburger');
 const backdrop = document.querySelector('#backdrop');
 
-btnOpenHamburger.addEventListener('click', () => {
-  showNav();
-});
+btnOpenHamburger.addEventListener('click', () => showNav());
 
-btnCloseHamburger.addEventListener('click', () => {
+btnCloseHamburger.addEventListener('click', () => controlNavClosing());
+
+backdrop.addEventListener('click', () => controlNavClosing());
+
+// Functions
+// // Makes nav visible
+const showNav = () => {
+  navMenu.classList.toggle('nav_open');
+  backdrop.classList.add('backdrop_show');
+};
+
+// // Hides nav
+const hideNav = () => {
+  navMenu.classList.remove('nav_open');
+  backdrop.classList.remove('backdrop_show');
+};
+
+// // Enables closing animation before hiding nav
+const controlNavClosing = () => {
   navMenu.classList.add('nav_closing');
   navMenu.addEventListener(
     'animationend',
@@ -20,14 +36,4 @@ btnCloseHamburger.addEventListener('click', () => {
     { once: true }
   );
   hideNav();
-});
-
-const showNav = () => {
-  navMenu.classList.toggle('nav_open');
-  backdrop.classList.add('backdrop_show');
-};
-
-const hideNav = () => {
-  navMenu.classList.remove('nav_open');
-  backdrop.classList.remove('backdrop_show');
 };
